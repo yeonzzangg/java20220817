8283df00eabfde6780b4ca61fd0800178d28e754
yeonzzangg <110441635+yeonzzangg@users.noreply.github.com>
2022-08-17 12:13:41 +0900

    first commit

---
+++ src/ch01/book/Hello.java
@@ -0,0 +1,9 @@
+package ch01.book;
+
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Welcome to the java world!");
+	} //end of main
+} //end of class
---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,23 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args) {
+		// 주석 : 프로그램 실행에 영향을 미치지 않음
+		// 한줄 주석
+		/*
+		 * 여러줄 주석
+		 * 여러줄 주석
+		 */
+		// 주석 단축키 : ctrl + /
+		
+		// 힌트얻기 : ctrl + space
+		// 한 줄 지우기 : ctrl + d
+		// 코드 위아래로 옮기기 : alt + up or down
+
+		// 콘솔 출력 코드 : sysout / sout
+		
+		System.out.println("Hello World!");
+//		System.out.println("자바는 쉽지 않다");
+		System.out.println("잘 배워서 써먹어 보자");
+	}
+}
