ef006f539d5044c8b064c6f6f4eb36995142df09
yeonzzangg <110441635+yeonzzangg@users.noreply.github.com>
2022-09-30 17:48:19 +0900

    test

---
+++ src/java20220817/Student.java
@@ -0,0 +1,13 @@
+package java20220817;
+
+public class Student extends choongang {
+	int num;
+	
+	public Student(int n, int hp){
+		this.num = n;
+		this.hp = hp;
+	}
+	
+	public String q() {
+	}
+}
---
+++ src/java20220817/choongang.java
@@ -0,0 +1,25 @@
+package java20220817;
+
+public class choongang {
+	int hp;
+	
+	
+	// constructors
+	
+	
+	
+	// method
+	void setHP(int hp) {
+		this.hp = hp;
+	}
+	
+	public void rest() {
+		if(hp>50) {
+			hp =100;
+		}else {
+			
+		}
+	}
+}
+
+
